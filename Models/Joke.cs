f11fef01db61d5994b7325d2f80885d6f1a6d314
Tamás Vicze <tamasvicze@hotmail.com>
2022-09-12 15:17:40 +0200

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWebApplication.Models
+{
+    public class Joke
+    {
+        public int ID { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
